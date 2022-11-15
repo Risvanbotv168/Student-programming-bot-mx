3d02750b956ab2931aeb8d0b7594b135c0645c75
Risvanbotv168 <118274533+Risvanbotv168@users.noreply.github.com>
2022-11-15 20:54:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,26 +13,28 @@ MALLU_FILTER = Client(
 
 @MALLU_FILTER.on_message(filters.command("start"))
 async def start_cmd(client‚message):
-    print("START Command")
+    await message.reply_text("Hello..! My name is mallu filter bot , i can provide movies ,  just  add  me to  group an    And one more thing, I'm a filter bot😇")
 
 
 @MALLU_FILTER.on_message(filters.command("help"))
 async def help_cmd(client‚message):
-    print("HELP Command")
+    await message.reply_text("Please personal message @Riwan_x")
 
 @MALLU_FILTER.on_message(filters.command("settings"))
 async def settings_cmd(client‚message):
-    print("SETTINGS Command")
+    await message.reply_text("I'm not connected to any groups!")
 
 @MALLU_FILTER.on_message(filters.command("connect"))
 async def connect_cmd(client‚message):
-    print("CONNECT Command")
+    await message.reply_text("Enter in correct format!
+
+/connect groupid
+
+Get your Group id by adding this bot to your group and use  /id
 
 @MALLU_FILTER.on_message(filters.command("filter"))
 async def filter_cmd(client‚message):
-    print("FILTER Command")
-
-
+    await message.reply_text("I'm not connected to any groups!")
 
 
 
