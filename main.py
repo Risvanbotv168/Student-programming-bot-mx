3d245f6fd6b50d87e9ac5506c48f9cd6635efb0f
Risvanbotv168 <118274533+Risvanbotv168@users.noreply.github.com>
2022-11-15 19:51:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-from pyrogram import Client
+from pyrogram import Client‚ filters
+
+API_ID = "10874827"
+API_HASH = "b31a3277eaa54329f0c183ab769319be"
+BOT_TOKEN = "5682753013:AAGNKu2DCp2w8OjpUapmDwxcKbZzvVbR1MI"
+
+MALLU_FILTER = Client(
+   name="MALLU_FILTER"
+   api_id=API_ID‚
+   api_hash=API-HASH‚
+   bot_token=BOT_TOKEN‚
+)
+
+print("Bot Started")
+
+MALLU_FILTER.run()
